72b4b243d82de87516d82ffb2dd5f518a91bf42d
Alan Ciccotelli <alanciccotelli9@gmail.com>
2019-12-11 18:27:04 -0300

    removed reminder from initial state

--- src/reducers/remindersReducer.js
+++ src/reducers/remindersReducer.js
@@ -2,32 +2,7 @@ import {ADD_REMINDER, GET_REMINDERS_BY_ID} from '../actions/types';
 import moment from 'moment';
 
 const initialState = {
-    reminder: {
-        dayId: null,
-        title: "",
-        time: "",
-        city: "",
-        weather : "",
-        color: ""
-    },
-    allReminders: [
-        {
-            date : '10/12/2019',
-            title :"Reminder",
-            time :"20:10",
-            city :"London",
-            Weather: "Rain",
-            color :"red"
-        },
-        {
-            dayId :10,
-            title :"Reminder",
-            time :"20:15",
-            city :"Wales",
-            Weather: "Rain",
-            color :"red"
-        }
-    ]
+    allReminders: []
 }
 
 export default function(state = initialState, action) {
