8730190d70e534e66cb059f5c729fbc000334688
Alan Ciccotelli <alanciccotelli9@gmail.com>
2019-12-11 19:15:16 -0300

    added modal to reminder

--- src/Components/Reminder.js
+++ src/Components/Reminder.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Modal from '@material-ui/core/Modal';
 
 export default class Reminder extends Component {
     constructor(props) {
@@ -7,15 +8,29 @@ export default class Reminder extends Component {
             title: props.reminder.title,
             dateTime: props.reminder.dateTime,
             city: props.reminder.city,
-            color: props.reminder.color 
+            color: props.reminder.color,
+            isModalOpen: false
         }
     }
     reminderDetails () {
+        this.openModal();
         console.log(this.state);
     }
 
+    openModal() {
+        this.setState({
+            isModalOpen: true
+        });
+    }
+
+    closeModal() {
+        this.setState({
+            isModalOpen: false
+        });
+    }
+
     render() {
-        const {title, dateTime, city, color} = this.state;
+        const {title, dateTime, city, color, isModalOpen} = this.state;
         const reminderStyle = {
                         background: color,
                         border: "1px",
@@ -25,6 +40,15 @@ export default class Reminder extends Component {
         return (
             <div key={`${dateTime}-${title}`} style={reminderStyle} onClick={this.reminderDetails.bind(this)}>
                 {title}
+                <Modal
+                    aria-labelledby="simple-modal-title"
+                    aria-describedby="simple-modal-description"
+                    open={isModalOpen}
+                    onClose={this.closeModal.bind(this)}
+                >
+                    <h1>Hola</h1>
+                </Modal>
+                {city}
             </div>
         )
     }
