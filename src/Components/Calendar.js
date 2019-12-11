c819df9b0721dc3b67ea807e1acf8eb490760a3f
Alan Ciccotelli <alanciccotelli9@gmail.com>
2019-12-11 18:23:48 -0300

    Added reminder form

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import Month from './Month';
-import moment from 'moment';
+import ReminderForm from './ReminderForm';
 
 export default class Calendar extends Component {
     render() {
         return (
             <div>
+               <ReminderForm/>
                <h2 className="title">Calendar</h2>
                <Month/>
             </div>
