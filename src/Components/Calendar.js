f7ceec2490b21c8d2125e9e3d27e2fdcf4db7b1e
Alan Ciccotelli <alanciccotelli9@gmail.com>
2019-12-12 14:39:29 -0300

    Moved Reminder Form inside the month

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
 import Month from './Month';
 import ReminderForm from './ReminderForm';
+import moment from 'moment';
 
 export default class Calendar extends Component {
     render() {
         return (
             <div>
-                <ReminderForm inModal={false} />
-                <h2 className="title">Calendar</h2>
-                <Month />
+                <Month/>
             </div>
         )
     }
