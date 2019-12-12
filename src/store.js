84248f9cf07caf3e4d11c02b498e9ddd316b05af
Alan Ciccotelli <alanciccotelli9@gmail.com>
2019-12-11 21:29:46 -0300

    condition for redux developer tool

--- src/store.js
+++ src/store.js
@@ -1,9 +1,18 @@
-import {createStore, compose, applyMiddleware} from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 
 const middleware = [thunk];
 const initialState = {};
-const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
-
+let store;
+// Only chrome can handle the redux dev tool
+// redux compose cannot handle a null or undefined middleware
+store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+        applyMiddleware(...middleware),
+        (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()) || compose
+    )
+);
 export default store;
